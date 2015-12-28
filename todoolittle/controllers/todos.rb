b79ec38907b3898251b968a9d4d1c83bcd2a4a21
Bernice Anne W. Chua <email": null>
2015-12-27 19:43:49 -0800

    Complete 'Snitching On Sinatra'.

---
+++ todoolittle/controllers/todos.rb
@@ -0,0 +1,17 @@
+# get '/todos' do
+#   "Welcome to todoolittle!"
+# end
+
+get '/todos' do
+  @todos = Todo.all
+  erb :index
+end
+
+# post '/todos' do
+#   "Hello from POSTS /todos!!"
+# end
+
+post '/todos' do
+  Todo.create(description: params[:description])
+  redirect '/todos'
+end
